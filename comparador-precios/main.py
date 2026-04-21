65549d3fd5e1a1935e1677e3553d89cc917a579c
koamis <koamis@gmail.com>
2026-04-21 06:47:22 -0600

    Delete comparador-precios directory

--- comparador-precios/main.py
+++
@@ -1,8 +0,0 @@
-# Asegúrate de que esta parte esté así en tu main.py para que Railway asigne el puerto
-import os
-import uvicorn
-
-if __name__ == "__main__":
-    # Railway asigna un puerto dinámico, esto lo captura
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
